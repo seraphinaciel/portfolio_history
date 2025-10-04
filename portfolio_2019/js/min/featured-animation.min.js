23f38ee19833aca59d78b5b8e6abc67f8f65c70e
seraphinaciel <seraphinaciel@gmail.com>
2025-10-04 14:00:24 +0900

    2019

---
+++ portfolio_2019/js/min/featured-animation.min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("[rel='tooltip']").tooltip(),$(".thumbnail").hover(function(){$(this).find(".caption").slideDown(250)},function(){$(this).find(".caption").slideUp(250)})});
